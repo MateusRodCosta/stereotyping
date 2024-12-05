98ce07e7e6dd9b0e92b2fe28812c3af02ddd2fd5
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-12-05 11:12:38 -0300

    Improve UI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod stereotype;
 
-use adw::{ActionRow, Application, ApplicationWindow, HeaderBar, ToolbarView};
+use adw::{ActionRow, Application, ApplicationWindow, Clamp, HeaderBar, ToolbarView};
 use glib::clone;
 use gtk::prelude::*;
 use gtk::{gio, glib};
-use gtk::{Box, Button, Label, ListBox};
-use gtk::{Orientation, Align};
-use gtk::FileDialog;
-use stereotype::{stereotyper::stereotype_file, file_stereotype::FileStereotype};
+use gtk::{Align, Orientation};
+use gtk::{Box, Button, FileDialog, ListBox, ScrolledWindow};
+use stereotype::{file_stereotype::FileStereotype, stereotyper::stereotype_file};
 
 const APP_ID: &str = "dev.mateusrodcosta.Stereotyping";
 
@@ -22,12 +21,7 @@ fn main() -> glib::ExitCode {
 fn build_intial_screen(app: &Application) {
     let header_bar = HeaderBar::builder().build();
 
-    let label = Label::builder().label("Stereotyping").build();
-    label.add_css_class("heading");
-
-    let button = Button::builder()
-        .label("Choose File...")
-        .build();
+    let button = Button::builder().label("Choose File...").build();
     button.add_css_class("suggested-action");
     button.add_css_class("pill");
     button.set_vexpand(false);
@@ -35,15 +29,14 @@ fn build_intial_screen(app: &Application) {
     button.set_can_shrink(true);
 
     let content = Box::builder()
-    .margin_top(32)
-    .margin_bottom(32)
-    .margin_start(32)
-    .margin_end(32)
-    .orientation(Orientation::Vertical)
-    .valign(Align::Center)
-    .build();
+        .margin_top(32)
+        .margin_bottom(32)
+        .margin_start(32)
+        .margin_end(32)
+        .orientation(Orientation::Vertical)
+        .valign(Align::Center)
+        .build();
     content.set_spacing(16);
-    content.append(&label);
     content.append(&button);
 
     let toolbar_view = ToolbarView::builder().content(&content).build();
@@ -56,15 +49,29 @@ fn build_intial_screen(app: &Application) {
         .build();
     window.set_default_size(640, 480);
 
-    button.connect_clicked(clone!(@weak window, @weak toolbar_view => move |_| {
-        let file_dialog = FileDialog::new();
-        file_dialog.open(Some(&window), None::<&gio::Cancellable>, clone!(@weak window => move |r| {
-            match r {
-                Ok(file) => fetch_file_data(file, &window, &toolbar_view),
-                _ => (),
-            }
-        }));
-    }));
+    button.connect_clicked(clone!(
+        #[weak]
+        window,
+        #[weak]
+        toolbar_view,
+        move |_| {
+            let file_dialog = FileDialog::new();
+            file_dialog.open(
+                Some(&window),
+                None::<&gio::Cancellable>,
+                clone!(
+                    #[weak]
+                    window,
+                    move |r| {
+                        match r {
+                            Ok(file) => fetch_file_data(file, &window, &toolbar_view),
+                            _ => (),
+                        }
+                    }
+                ),
+            );
+        }
+    ));
 
     window.present();
 }
@@ -89,67 +96,72 @@ fn fetch_file_data(file: gio::File, window: &ApplicationWindow, toolbar_view: &T
             let filename = &s.get_filename();
             window.set_title(Some(&format!("Stereotyping - {}", filename)));
             toolbar_view.set_content(Some(&content));
-        },
-        _ => ()
+        }
+        _ => (),
     };
-
 }
 
-fn build_details_screen(stereotype: &FileStereotype) -> Box {
+fn build_details_screen(stereotype: &FileStereotype) -> ScrolledWindow {
     let list_items = stereotype_to_rows(stereotype);
-
-    let box_label = Label::builder().label("File Stereoype").build();
-    box_label.add_css_class("heading");
-    box_label.set_halign(Align::Start);
-
-    let listbox = ListBox::builder()
-    .build();
+    let listbox = ListBox::builder().build();
     listbox.add_css_class("boxed-list");
-
     for item in list_items.iter() {
         listbox.append(item);
     }
 
     let content = Box::builder()
-    .orientation(Orientation::Vertical)
-    .margin_top(32)
-    .margin_bottom(32)
-    .margin_start(32)
-    .margin_end(32)
-    .build();
-    content.append(&box_label);
-    content.append(&listbox);
+        .margin_top(32)
+        .margin_bottom(32)
+        .margin_start(32)
+        .margin_end(32)
+        .orientation(Orientation::Vertical)
+        .valign(Align::Center)
+        .build();
     content.set_spacing(16);
-    content.set_valign(Align::Center);
+    content.append(&listbox);
 
-    content
+    let clamp = Clamp::builder().child(&content).build();
+    let scrolled_window = ScrolledWindow::builder().child(&clamp).build();
+
+    scrolled_window
 }
 
 fn stereotype_to_rows(stereotype: &FileStereotype) -> [ActionRow; 5] {
     let item_filename = ActionRow::builder()
-    .title("Filename").subtitle(stereotype.get_filename())
-    .build();
+        .title("Filename")
+        .subtitle(stereotype.get_filename())
+        .build();
     item_filename.add_css_class("property");
 
     let item_description = ActionRow::builder()
-    .title("Description").subtitle(stereotype.get_description())
-    .build();
+        .title("Description")
+        .subtitle(stereotype.get_description())
+        .build();
     item_description.add_css_class("property");
 
     let item_mime_type = ActionRow::builder()
-    .title("MIME Type").subtitle(stereotype.get_mime_type())
-    .build();
+        .title("MIME Type")
+        .subtitle(stereotype.get_mime_type())
+        .build();
     item_mime_type.add_css_class("property");
 
     let item_mime_encoding = ActionRow::builder()
-    .title("MIME Encoding").subtitle(stereotype.get_mime_encoding())
-    .build();
+        .title("MIME Encoding")
+        .subtitle(stereotype.get_mime_encoding())
+        .build();
     item_mime_encoding.add_css_class("property");
 
     let item_extension = ActionRow::builder()
-    .title("Extension").subtitle(stereotype.get_entension())
-    .build();
+        .title("Extension")
+        .subtitle(stereotype.get_entension())
+        .build();
     item_extension.add_css_class("property");
 
-    [item_filename, item_description, item_mime_type, item_mime_encoding, item_extension]
+    [
+        item_filename,
+        item_description,
+        item_mime_type,
+        item_mime_encoding,
+        item_extension,
+    ]
 }
